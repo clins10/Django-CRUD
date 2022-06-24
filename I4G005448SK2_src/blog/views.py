4f9e38ac24fbe418e5b61bf108c3ff155c770e93
Viashima <72166830+clins10@users.noreply.github.com>
2022-06-24 20:32:13 +0100

    added pagination

--- I4G005448SK2_src/blog/views.py
+++ I4G005448SK2_src/blog/views.py
@@ -1,16 +1,23 @@
-from telnetlib import STATUS
 from django.urls import reverse_lazy
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from blog.models import Post
+from blog.models import Post 
 
+
+# from django.contrib.auth.models import Post
+# from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 # from I4G005448SK2_src.blog.models import Post
 
 
 # creating classes
 class PostListView(ListView):
     model = Post
+    template_name = 'blog/post_list.html'
+    # context_object_name = 'blog'
+    paginate_by = 3
+    queryset: Post.objects.all()
+
     # def get_queryset(self):
     #     return super().queryset().filter(STATUS = 'Published')
     # paginate_by: 3
